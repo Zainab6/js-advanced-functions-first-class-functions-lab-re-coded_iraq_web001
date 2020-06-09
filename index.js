a0c59c74590bb0251015361e4b8970159a1bee24
Zainab Saud <zai99nb@gmail.com>
2020-06-09 10:31:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const drivers1 = drivers.slice(0,2);
+
+const drivers2 = drivers.slice(2,4);
+
+
+function returnFirstTwoDrivers(drivers){
+   return drivers1
+  }
+function returnLastTwoDrivers(drivers){
+  return drivers2
+}
+ selectingDrivers = [returnFirstTwoDrivers(),returnLastTwoDrivers()];
+   
+createFareMultiplier(a){
+return fareQuintupler(a){
+  a = a * 4;
+  return fareDoubler(a){
+    a = a * 2;
+    return fareTripler(a){
+      a = a * 3 ;
+      return a 
+    }
+  }
+}
+}
